6406fd6d9e9457eaf93939ac55deb3aaf235e559
Nima dandu lama <dandulama02@gmail.com>
2025-05-06 06:53:00 +0545

    adding animation

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById("hamburger");
+const navlinks = document.getElementById("navlinks");
+
+hamburger.addEventListener('click' , () => {
+    navlinks.classList.toggle('active');
+});
+
+const text ="Hi, I'm Mr.s | ";
+const typeText = document.getElementById('typed-text');
+let index = 0;
+function type(){
+    if(index < text.length){
+        typeText.textContent += text.charAt(index);
+        index++;
+        setTimeout(type,150);
+    }
+}
+type();
