68293d68afa2523d819d0c92e26eabe384af44f5
Nima dandu lama <dandulama02@gmail.com>
2025-05-08 07:33:24 +0545

    modifiying the card using js

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function type(){
 }
 type();
 
+const projects = [
+{
+    image : "./image/pexels-cody-berg-463684.jpg",
+    title : " Animated Navbar",
+    description : "A responsive navigation bar with smooth animation and"
+},
+{
+    image : "./image/pexels-cody-berg-463684.jpg",
+    title : " Animated Navbar",
+    description : "A responsive navigation bar with smooth animation and" 
+},
+{
+image : "./image/pexels-cody-berg-463684.jpg",
+title : " Animated Navbar",
+description : "A responsive navigation bar with smooth animation and"
+},
+];
+const container = document.getElementById("project-container");
+projects.forEach(project=> {
+
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+    <img src = "${project.image}" alt = "${project.title}"/>
+    <h3> ${project.title}</h3>
+    <p>${project.description}</p>
+    `;
+    container.appendChild(card)
+})
